c053c78279964c7dfd50af66239a784c700fc859
UnweidlyKhan349 <henrylu13306@gmail.com>
2024-12-23 17:34:48 -0500

    setuo

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
+/*
+"Galactic Rap " Kevin MacLeod (incompetech.com)
+Licensed under Creative Commons: By Attribution 4.0 License
+http://creativecommons.org/licenses/by/4.0/
+*/
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
-    window.setFramerateLimit(144);
+int main() {
+    sf::RenderWindow window = sf::RenderWindow(sf::VideoMode({800, 600}), "Game");
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+    sf::Music music("galactic_rap.mp3");
+    music.play();
+
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
